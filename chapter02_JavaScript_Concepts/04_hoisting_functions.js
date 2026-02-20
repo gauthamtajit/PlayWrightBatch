01995bdb0d9fec04abbff379376dedcf0e6aa9b3
Gautham <gauthamtajit@gmail.com>
2026-02-21 01:25:10 +0530

    Add hosting and let examples from chapter 2

--- chapter02_JavaScript_Concepts/04_hoisting_functions.js
+++ chapter02_JavaScript_Concepts/04_hoisting_functions.js
@@ -1,7 +1,13 @@
-//Definition of functions
+// Step1: Definition of functions
 function getUserStatus() {
-
-
-
-
+    console.log(stat_code);
+    var stat_code = 200;
+    console.log(stat_code);
 }
+
+// Step 2: Calling of functions
+getUserStatus();
+
+console.log(x);
+let x = 10;
+console.log(x);
---
+++ chapter02_JavaScript_Concepts/05_let_Example.js
@@ -0,0 +1,11 @@
+let var1 = 10;
+console.log(var1);
+
+if (true) {
+    //console.log(var1);
+    let var1 = 20;
+    console.log(var1);
+}
+
+console.log(var1);
+
